d8ae477a63c108a85e6c2ea677f7d41d3478707e
Vyacheslav Frolov <frolov78@gmail.com>
2021-11-05 13:49:54 +0000

    Rename binaries for x86_64

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ mkdir build
 go clean
 go test ./...
 
-GOOS=linux  GOARCH=amd64 go build -v -o build/goFileServer_linux64   goFileServer.go
-GOOS=linux  GOARCH=arm64 go build -v -o build/goFileServer_arm64     goFileServer.go
-GOOS=darwin GOARCH=amd64 go build -v -o build/goFileServer_mac64     goFileServer.go
-GOOS=darwin GOARCH=arm64 go build -v -o build/goFileServer_mac_arm64 goFileServer.go
+GOOS=linux  GOARCH=amd64 go build -v -o build/goFileServer_linux_x86_64 goFileServer.go
+GOOS=linux  GOARCH=arm64 go build -v -o build/goFileServer_linux_arm64  goFileServer.go
+
+GOOS=darwin GOARCH=amd64 go build -v -o build/goFileServer_mac_x86_64   goFileServer.go
+GOOS=darwin GOARCH=arm64 go build -v -o build/goFileServer_mac_arm64    goFileServer.go
